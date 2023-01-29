88190cc5eb2b1ae161050c8a14c8c530934b7210
RyosukeGF <ryosep175@gmail.com>
2023-01-30 00:41:59 +0900

    firstcommit

---
+++ src/Review01.java
@@ -0,0 +1,24 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+
+        int product = 500;
+        int percent = 10;
+
+        int tamount = tax(500, 10);
+
+        //int pamount = product + tamount;
+
+
+        System.out.println(product + "円の商品の税込価格は" +(product+tamount) +"円（消費税は" +tamount + "円)です。");
+
+    }
+
+    public static int tax(int product, int percent) {
+        int result = product / percent;
+        return result;
+    }
+
+}
